d6f721b653c940a4a0d2fc0826643b49d225a0d8
wtanil <tanilwilliam@gmail.com>
2019-07-02 13:30:02 +0700

    Implement tag factory

---
+++ database/factories/TagFactory.php
@@ -0,0 +1,34 @@
+<?php
+
+use Faker\Generator as Faker;
+
+/*
+|--------------------------------------------------------------------------
+| Model Factories
+|--------------------------------------------------------------------------
+|
+| This directory should contain each of the model factory definitions for
+| your application. Factories provide a convenient way to generate new
+| model instances for testing / seeding your application's database.
+|
+*/
+
+$factory->define(App\tag::class, function (Faker $faker) {
+    return [
+        'name' => $faker->word,
+
+        'priority' => $faker->numberBetween(1, 10),
+        'color' => $faker->randomElement([
+            'FFFFFF',
+            '000000',
+            'FF0000',
+            '00FF00',
+            '0000FF',
+            'FFFF00',
+            '00FFFF',
+            'FF00FF'
+        ]),
+        'hidden' => true
+    ];
+});
+
