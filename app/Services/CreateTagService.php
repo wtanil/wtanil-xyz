830061b32b929bf94fe705c3b35fed8b06d5fb9f
wtanil <tanilwilliam@gmail.com>
2019-07-02 14:08:20 +0700

    Create CreateTagService

---
+++ app/Services/CreateTagService.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Services;
+
+use Illuminate\Support\Facades\Auth;
+
+class CreateTagService {
+
+    /**
+     *  Contructor
+     *
+     *  @param
+     *  @return void
+     */
+    public function __construct() {
+
+    }
+
+    
+}
