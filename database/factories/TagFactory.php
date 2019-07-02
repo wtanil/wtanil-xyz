acb9178fde11f6939fa461dc23bfa04ddadaa503
wtanil <tanilwilliam@gmail.com>
2019-07-02 13:31:35 +0700

    Fix class name typo in tag factory

--- database/factories/TagFactory.php
+++ database/factories/TagFactory.php
@@ -13,7 +13,7 @@ use Faker\Generator as Faker;
 |
 */
 
-$factory->define(App\tag::class, function (Faker $faker) {
+$factory->define(App\Tag::class, function (Faker $faker) {
     return [
         'name' => $faker->word,
 
