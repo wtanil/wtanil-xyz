129ac5847d27b39d2413222f12458ca59c8c2d10
wtanil <tanilwilliam@gmail.com>
2019-07-05 15:27:46 +0700

    Implement create tag page

--- resources/views/tags/create.blade.php
+++ resources/views/tags/create.blade.php
@@ -3,12 +3,44 @@
 @section('content')
 <div class="container">
 
-<form method="POST" action="/tags" class="col-sm">
-    @csrf
+    <div class="row">
+
+        <div class="col">
+
+            <form method="POST" action="/tags">
+                @csrf
+
+                <!-- Name -->
+                <div class="form-group">
+                    <label for="name">Name</label>
+                    <input type="text" name="name" id="name" class="form-control">
+                </div>
+
+                <!-- Priority -->
+                <div class="form-group">
+                    <label for="priority">Priority</label>
+                    <input type="text" name="priority" id="priority" class="form-control">
+                </div>
+
+                <!-- Color -->
+                <div class="form-group">
+                    <label for="color">Color</label>
+                    <input type="text" name="color" id="color" class="form-control">
+                </div>
+
+                <!-- Add -->
+                <div class="form-group">
+                    <div class="">
+                        <button type="submit" class="btn btn-primary">
+                            <i class="fa fa-plus"></i> Add
+                        </button>
+                    </div>
+                </div>
 
 
-    
-</form>
+            </form>
+        </div>
+    </div>
 
 </div>
 @endsection
