ba59bb823124b28b17528f263e6bdb4e0f192cae
wtanil <tanilwilliam@gmail.com>
2019-07-11 16:16:49 +0700

    Add project model, controller, and migration
    Make project migration
    
    Add fillable property of project model

---
+++ database/migrations/2019_07_11_082645_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('name', 190);
+            $table->text('description');
+            $table->boolean('hidden')->default(true);
+            $table->date('start_date');
+            $table->date('last_update_date');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+}
