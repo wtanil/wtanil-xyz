c258be7a4077a36a4a69a2e2418b13ec32af7406
wtanil <tanilwilliam@gmail.com>
2019-07-12 12:00:02 +0700

    Implement create method on CreateProjectService
    Fix typo on CreateProjectServiceTest

--- tests/Unit/CreateProjectServiceTest.php
+++ tests/Unit/CreateProjectServiceTest.php
@@ -6,7 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
-use App\Tag;
+use App\Project;
 use App\Services\CreateProjectService;
 
 class CreateProjectServiceTest extends TestCase
