44858ef86705bfcd32e25d1f231ab6c2ed1ecad4
wtanil <tanilwilliam@gmail.com>
2019-07-13 09:41:59 +0700

    Implement getAll method of ProjectQueryService

---
+++ app/Services/ProjectQueryService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+
+use App\Project;
+
+class ProjectQueryService {
+
+    /**
+     *  Contructor
+     *
+     *  @param
+     *  @return void
+     */
+    public function __construct() {
+
+    }
+
+
+    /**
+     *  Get all projects
+     *
+     *  @param  
+     *  @return Collection 
+     */
+    public function getAll() {
+
+        return Project::orderBy('started_date', 'desc')->get();
+
+    }
+
+
+}
