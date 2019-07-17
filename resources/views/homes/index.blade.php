4780cb37a64ea79746759c2f8844b8bcfb683d65
wtanil <tanilwilliam@gmail.com>
2019-07-17 14:20:22 +0700

    Write home index page

--- resources/views/homes/index.blade.php
+++ resources/views/homes/index.blade.php
@@ -1,23 +1,46 @@
 @extends('layouts.app')
 
 @section('content')
+
 <div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">Dashboard</div>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+    INDEX
 
-                    You are logged in!
-                </div>
-            </div>
-        </div>
-    </div>
+    <table class="table table-stripped table-bordered">
+        <thead>
+            <tr>
+                <th scope="col">#</th>
+                <th scope="col">Name</th>
+                <th scope="col">Start Date</th>
+                <th scope="col">Last Update Date</th>
+            </tr>
+        </thead>
+        <tbody>
+
+            @if ($projects->isNotEmpty())
+            @foreach ($projects as $project)
+
+            <tr>
+
+                <th scope="row">{{ $loop->iteration }}</th>
+                <td>
+                    {{ $project->name }}
+                </td>
+                <td>
+                    {{ $project->start_date }}
+                </td>
+                <td>
+                    {{ $project->last_update_date }}
+                </td>
+                
+            </tr>
+
+            @endforeach
+            @endif
+
+        </tbody>
+    </table>
+
 </div>
+
 @endsection
