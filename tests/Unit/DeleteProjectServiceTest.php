066f6b523f53c1e6a6da9dd1b46577631830c439
wtanil <tanilwilliam@gmail.com>
2019-07-20 14:41:05 +0700

    Implement unit test for delete method of DeleteProjectService

--- tests/Unit/DeleteProjectServiceTest.php
+++ tests/Unit/DeleteProjectServiceTest.php
@@ -6,15 +6,43 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+use App\Project;
+use App\Services\DeleteProjectService;
+
 class DeleteProjectServiceTest extends TestCase
 {
-    /**
-     * A basic unit test example.
-     *
-     * @return void
-     */
-    public function testExample()
+    use RefreshDatabase;
+
+    protected $deleteProjectService;
+    protected $factoryProjects;
+
+    protected function setUp(): void
     {
-        $this->assertTrue(true);
+
+        parent::setUp();
+
+        $this->deleteProjectService = $this->app->make(DeleteProjectService::class);
+
+        $this->factoryProjects = factory(Project::class, 5)->create();
+
+    }
+
+
+    /**
+     *  @test
+     *  @group UnitDeleteProjectService
+     */
+    public function delete_ValidRequirements_ProjectIsDeleted()
+    {
+        // Arrange
+        $project = $this->factoryProjects->first();
+        $id = $project->id;
+
+        // Act
+        $deleted = $this->deleteProjectService->delete($id);
+
+        // Assert
+        $this->assertEquals(1, $deleted);
+        $this->assertDatabaseMissing('projects', $project->toArray());
     }
 }
