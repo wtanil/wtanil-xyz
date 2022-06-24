c0b47d89d7cd19cd4426f89d6ae28dc5b0ba05b4
wtanil <tanilwilliam@gmail.com>
2022-06-24 14:30:04 +0700

    Improve UI

--- resources/views/layouts/portfolio.blade.php
+++ resources/views/layouts/portfolio.blade.php
@@ -23,7 +23,7 @@
 </head>
 <body>
     <div id="app">
-        <nav class="navbar navbar-expand-md navbar-dark bg-dark">
+        <nav class="navbar navbar-expand-md navbar-dark bg-darkgreen">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
                     {{ config('app.name', 'William') }}
